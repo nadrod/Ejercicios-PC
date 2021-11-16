5d7b23587a7e6bfdbc66245615234d32e9d9e37e
jose pablo <jose.perezh@uanl.edu.mx>
2021-11-15 21:05:29 -0600

    Create Usb_File_Copy (1).ps1

---
+++ Python/E13_CombinaciónLenguajes/Usb_File_Copy (1).ps1
@@ -0,0 +1,15 @@
+﻿
+function Get-Usb{
+    $drives = [System.IO.DriveInfo]::GetDrives()
+    $r = $drives | Where-Object { $_.DriveType -eq 'Removable' -and $_.IsReady }
+    if ($r) {
+        return @($r)[-1]
+    }
+    throw "No removable drives found."
+}
+
+$usb = Get-Usb
+$usb.RootDirectory.Name
+
+New-Item -Path C:\Users\HP\Desktop\Usb_Copy\ -Force 
+Copy-Item -Path $usb.RootDirectory.Name -Destination C:\Users\HP\Desktop\Usb_Copy\ -Recurse -Force -Passthru
